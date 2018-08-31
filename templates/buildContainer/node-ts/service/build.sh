98aceb4fcfc9c55b685d6bfba7115da2e8bfad09
Unknown <djqballer@outlook.com>
2018-08-31 13:31:26 -0600

    Various template fixes

--- templates/buildContainer/node-ts/service/build.sh
+++ templates/buildContainer/node-ts/service/build.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-# Map typescript's output directory to our output directory
-mkdir -p dist
-mount --bind /output dist
-
 # Make sure node packages are installed
 npm i
 
 # Build the typescript code
 npm run build
-
-# Unmap directory
-umount dist
