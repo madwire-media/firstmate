2deb13eb390f1eaf9a94c498bd3bb0fc00b9acc7
Dakota Sullivan <djqballer@outlook.com>
2018-09-14 07:59:41 -0600

    Give the node-ts buildContainer package-lock.json hashing

--- templates/buildContainer/node-ts/service/build.sh
+++ templates/buildContainer/node-ts/service/build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-# Make sure node packages are installed
-npm i
+current_hash=$(md5sum package-lock.json | awk '{ print $1 }')
+last_hash=$(cat /cache/package-lock.json.md5)
+
+if [[ $current_hash != $last_hash ]]; then
+  # Make sure node packages are installed
+  npm i
+
+  # Save the new md5 hash
+  echo $current_hash > /cache/package-lock.json.md5
+fi
 
 # Build the typescript code
 npm run build
